62b9c56d07e01d8d6ff8762f5ec601a17b5cdb4d
Ethan Seiber <ethcseib@ut.utm.edu>
2020-01-24 02:18:23 +0000

    update

--- shell.c
+++ shell.c
@@ -7,46 +7,62 @@
 void ParseString(char*, char*);
 
 int main (int argc, char* argv[]){
-  
+  printf("%d", argc);
+  if(fork() == 0)
+    printf("child running\n");
+  else
+    printf("main running\n");
   if(argc == 1){
     
-    FILE *fptr;
+    FILE *fptr = NULL;
     char* commandStr;
-    char* command;
-    fptr = fopen(argv[1], r);
-
+    char* command = (char*) malloc(15 * 15 * sizeof(char));
+    fptr = fopen(argv[1], "r");
+  
     if(fptr != NULL){
 
-      commandStr = fgets(commandStr, MAX_INT, fptr);
-      ParseString(commandStr, command);
+      //fgets(commandStr, MAX_INT, fptr);
+      //ParseString(commandStr, command);
     }
     fclose(fptr);
+    //free(command);
   }
-  else{
+  /*else{
     
     int check = 0;
     int err = 0;
-    char *command = (char*) malloc(INT_MAX*sizeof(char));
+    char *command = (char*) malloc(MAX_INT*sizeof(char));
     while(check == 0){
       
       printf("INPUT: ");
-      fgets(command, INT_MAX, stdin);
+      fgets(command, MAX_INT, stdin);
 
       if(strcmp(command, "quit") == 0){
 	check = 1;
       }
       else{
-	err = system(command);
+	//err = system(command);
 	if(err = -1){
 	  check = 1;
 	}
       }
     }
-  }
+    }*/
 
   return(0);
 }
 
 void ParseString(char* commands, char* command){
 
+  int i = 0;
+  int x = 0;
+  while(commands[i] != 0){
+
+    //command[x][i] = commands[i];
+    if(commands[i] == ';'){
+
+      x+=1;
+    }
+    i+=1;
+  }
 }
