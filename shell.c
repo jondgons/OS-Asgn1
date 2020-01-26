5f89f0b406a2c2a150d8ee7138279f5b841c9f62
Ethan Seiber <ethcseib@ut.utm.edu>
2020-01-25 19:43:06 -0600

    Began implementation of a batch parser.

--- shell.c
+++ shell.c
@@ -1,31 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #define MAX_INT 2147483647
 
-void ParseString(char*, char*);
+char* ParseBatch(char*, char**);
 
 int main (int argc, char* argv[]){
-  printf("%d", argc);
-  if(fork() == 0)
-    printf("child running\n");
-  else
-    printf("main running\n");
-  if(argc == 1){
+  
+  if(argc == 2){
     
     FILE *fptr = NULL;
-    char* commandStr;
-    char* command = (char*) malloc(15 * 15 * sizeof(char));
+    //char commandStr[];
+    char* commandStr = (char*) malloc(150*sizeof(char*));
+    char** commandArr = (char**) malloc(15*150*sizeof(char));
     fptr = fopen(argv[1], "r");
   
     if(fptr != NULL){
 
-      //fgets(commandStr, MAX_INT, fptr);
-      //ParseString(commandStr, command);
+      fgets(commandStr, MAX_INT, fptr);
+      
+      ParseBatch(commandStr, commandArr);
+      
+      /*for(int i = 0; commandArr[i] != EOF; i++){
+	if(fork() == 0){
+	  execvp(commandArr[i]);
+	}
+	else{
+	  wait();
+	}
+	}*/
     }
     fclose(fptr);
-    //free(command);
+    free(commandArr);
   }
   /*else{
     
@@ -48,21 +57,38 @@ int main (int argc, char* argv[]){
       }
     }
     }*/
-
+  else
+    printf("file pointer null\n");
   return(0);
 }
 
-void ParseString(char* commands, char* command){
+char* ParseBatch(char* commands, char** commandList){
 
+  char tmp[2] = ";";
+  char* token = (char*) malloc(150*sizeof(char));
+
+  token = strtok(commands, tmp);
+  commandList[0] = token;
+  
   int i = 0;
-  int x = 0;
-  while(commands[i] != 0){
-
-    //command[x][i] = commands[i];
-    if(commands[i] == ';'){
-
-      x+=1;
-    }
-    i+=1;
+  while(token != NULL){
+    i++;
+    token = strtok(NULL, tmp);
+    commandList[i] = token;
   }
+  commandList[i+1] = "\0";
+  char tmp2[2];
+  int len;/*Current problem: Outer for loop runs too long.*/
+  for(int x = 0; commandList[x][0] != '\0'; x+=1){
+    printf("x %d\n", x);
+    len = strlen(commandList[x]);
+    for(int y = 0; y < len && y+1 < len && len >= 2; y+=2){
+      tmp[0] = commandList[x][y];
+      tmp[1] = commandList[x][y+1];
+      if(strcmp(tmp, "./") == 0){
+	printf("found exe\n");
+      }printf("y %d\n", y);
+    }printf("next it\n");
+  }printf("congrats! Left\n");
+  free(token);
 }
